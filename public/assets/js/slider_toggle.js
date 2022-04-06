9f7d09dc73557b579509128d4c64feb5b865fcf6
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-06 19:31:12 +0200

    add feature toggle slider in JQuery for exemple

--- public/assets/js/slider_toggle.js
+++ public/assets/js/slider_toggle.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#sliderToggle").click(function(){
         $("#slider").slideToggle();
-        $("#sliderSwitchButton").toggleClass('bi bi-toggle-on bi bi-toggle-off ');  
-    });
+        $("#sliderSwitchButton").toggleClass('bi bi-toggle-on bi bi-toggle-off');  
+        $("#toggleText").text($("#toggleText").text() == 'Display Slider' ? 'Hide Slider' : 'Display Slider');
+        });
 });
