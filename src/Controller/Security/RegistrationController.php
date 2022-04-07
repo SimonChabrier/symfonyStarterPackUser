bc6fe0156ebefc068fdaf701bd5430b3202a0566
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-08 00:21:44 +0200

    add info for gmail config in RegistrationController.php comments

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -80,9 +80,10 @@ class RegistrationController extends AbstractController
             ->text('Contenu du message que je veux recevoir quand un utilisateur s\'enregistre...'); 
             $mailer->send($test_email); 
          
-            //* for use gmail add : composer require symfony/google-mailer and configure it to use third party application
+            //* for use gmail make the command : composer require symfony/google-mailer 
+            //* and configure it to use third party application :
             //* https://support.google.com/a/answer/6260879?hl=fr#:~:text=Vous%20pouvez%20autoriser%20ou%20non,%40gmail.com%22).&text=Applications%20moins%20s%C3%A9curis%C3%A9es.,-Pour%20afficher%20l
-            //* update your env.local with : MAILER_DSN=gmail://monmail@gmail.com:MotdePasseSpécialFourniParGmail@default?verify_peer=0
+            //* Fianlly, update your env.local with : MAILER_DSN=gmail://monmail@gmail.com:MotdePasseSpécialFourniParGmail@default?verify_peer=0
 
             // here are instructions for autologin after registration
             return $userAuthenticator->authenticateUser(
